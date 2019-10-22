5172a440fa8a7b5b876552e56bc8d381402c7506
Thales Zago <thaleszago@outlook.com>
2019-10-22 07:06:03 -0300

    Telas Finalizadas

---
+++ src/spacebankoficial/SpaceBankOficial.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spacebankoficial;
+import GUI.spaceLogin;
+/**
+ *
+ * @author UserSoffti
+ */
+public class SpaceBankOficial {
+
+   
+    public static void main(String[] args) {
+        spaceLogin sl = new spaceLogin();
+        sl.setVisible(true);
+             
+    }
+    
+}
